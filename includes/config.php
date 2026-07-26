71a3a482c468803ba751bed14fc19c823f38ed85
KUN3H0 <67368609+BISM4RCK@users.noreply.github.com>
2026-07-26 22:59:35 +0800

    configphp update
    code sucks ass
    update to config cuz shit dont work well and im a dumbass

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,29 @@ date_default_timezone_set('Asia/Manila');
 
 define('APP_NAME', 'GOLDEN HOMES Subdivision');
 define('APP_SHORT', 'GOLDEN HOMES');
-define('BASE_URL', 'http://localhost/smart-gate');
+
+/*
+|--------------------------------------------------------------------------
+| Automatically detect host
+|--------------------------------------------------------------------------
+*/
+
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    ? "https://"
+    : "http://";
+
+$host = $_SERVER['HTTP_HOST'];
+
+// If your project folder changes, only edit this.
+$projectFolder = "/smart-gate";
+
+define('BASE_URL', $protocol . $host . $projectFolder);
+
+/*
+|--------------------------------------------------------------------------
+| Database
+|--------------------------------------------------------------------------
+*/
 
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
